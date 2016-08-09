23fcb1bbf18f29eca37df0cca71a421c10d77998
unknown <vishaldk@IN-RAC-W180.thedigitalgroup.com>
2016-08-09 17:01:36 +0530

    ++4 - unstable

--- app.js
+++ app.js
@@ -8,32 +8,50 @@ app.factory('posts', [function () {
     return o;
 }]);
 
-app.controller('MainCtrl', ['$scope','posts', function( $scope, posts ){
-    $scope.posts = posts.posts;
-            
-    $scope.addPost = function () {
-        if (!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({
-            title: $scope.title,
-            link: $scope.link,
-            upvotes: 0
-        });
-        $scope.title = '';
-        $scope.link = '';
-    };
-    
-    $scope.incrementUpvotes = function( post ){
-        post.upvotes += 1;
-    };
-}]);
-
-app.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider){
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    console.log('state provider.')
     $stateProvider
         .state('home', {
             url: '/home',
             templateUrl: '/home.html',
             controller: 'MainCtrl'
+        })
+
+        .state('posts', {
+            url: '/posts/{id}',
+            templateUrl: '/posts.html',
+            controller: 'PostsCtrl'
         });
-        
     $urlRouterProvider.otherwise('home');
 }]);
+
+app.controller('MainCtrl', ['$scope', 'posts', function ($scope, posts) {
+    $scope.posts = posts.posts;
+
+    $scope.addPost = function () {
+        if (!$scope.title || $scope.title === '') { return; }
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 0,
+            comments: [
+                { author: 'Joe', body: 'Cool post!', upvotes: 0 },
+                { author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0 }
+            ]
+        });
+        $scope.title = '';
+        $scope.link = '';
+    }
+
+    $scope.incrementUpvotes = function (post) {
+        post.upvotes += 1;
+    }
+}])
+
+app.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function ($scope, $stateParams, posts) {
+        $scope.post = posts.posts[$stateParams.id];
+}])
--- readme.txt
+++ readme.txt
@@ -3,3 +3,8 @@ https://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/
 
 -- config function --
 https://thinkster.io/a-better-way-to-learn-angularjs/config-function 
+
+
+
+todo: 1. package json for all packages used..
+npm install -g express-generator
