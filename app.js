b175ed534d3495f5ca737d277754e25360ad9710
unknown <vishaldk@IN-RAC-W180.thedigitalgroup.com>
2016-08-09 16:36:45 +0530

    '++3'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.factory('posts', [function () {
     // service body
@@ -25,4 +25,15 @@ app.controller('MainCtrl', ['$scope','posts', function( $scope, posts ){
     $scope.incrementUpvotes = function( post ){
         post.upvotes += 1;
     };
+}]);
+
+app.config(['$stateProvider','$urlRouterProvider', function ($stateProvider, $urlRouterProvider){
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl'
+        });
+        
+    $urlRouterProvider.otherwise('home');
 }]);
--- readme.txt
+++ readme.txt
@@ -1,2 +1,5 @@
 ---> https://thinkster.io/mean-stack-tutorial#wiring-everything-up
 https://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/
+
+-- config function --
+https://thinkster.io/a-better-way-to-learn-angularjs/config-function 
